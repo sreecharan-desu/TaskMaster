7625fd9dae245bd6e721c7530de16ea270c63c0c
sreecharan-desu <sreecharan309@gmail.com>
2024-07-28 16:37:21 +0530

    moved things around

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const adminRouter = express.Router();
 
+
+//(post) -end points
 adminRouter.post('/signup',(req,res)=>{
     //code here
     res.json({
@@ -8,8 +10,46 @@ adminRouter.post('/signup',(req,res)=>{
     })
 });
 
-
 adminRouter.post('/signin',(req,res)=>{
+    //code here
+    res.json({
+        msg : 'Signin'
+    })
+});
+
+//(get) -end points
+adminRouter.get('/details',(req,res)=>{
+    //gets the admin details
+
+    //code here
+    res.json({
+        msg : 'Signin'
+    })
+});
+
+
+//(delete) -end points
+adminRouter.delete('/deleteuser',(req,res)=>{
+    //code here
+    res.json({
+        msg : 'Signin'
+    })
+});
+
+
+//(put) -end points
+adminRouter.put('/update',(req,res)=>{
+    //updates the admin details
+
+    //code here
+    res.json({
+        msg : 'Signin'
+    })
+});
+
+
+//(delete) -end points
+adminRouter.delete('/deleteuser',(req,res)=>{
     //code here
     res.json({
         msg : 'Signin'

d0594359eac2fa04dfcce524535b8427ad5bd061
sreecharan-desu <sreecharan309@gmail.com>
2024-07-28 11:35:35 +0530

    backend hello

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,18 @@ const cors = require('cors');
 const app = express();
 const mainRouter = require('./routes/mainRoute')
 
+//backendurl : https://task-master-kohl.vercel.app/
+
 app.use(express.json());
 app.use(cors());
 app.use("/api/v1/",mainRouter);
+
+app.get('/',(req,res)=>{
+    res.json({
+        msg : "Hello from backend"
+    })
+})
+
 //Test Comment
 
 app.listen(5000,(req,res)=>{

009dddad829e13e1dea6dae561e4496aaf480240
sreecharan-desu <sreecharan309@gmail.com>
2024-08-02 16:08:06 +0530

    cors error

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,12 @@ const cors = require('cors');
 const mainRouter = require('./routes/mainRoute')
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin : 'https://task-master-rose.vercel.app/',
+    methods : ('GET','POST','PUT','DELETE'),
+    credentials : true
+}));
+
 //backendurl (deployment): https://task-master-kohl.vercel.app/
 app.get('/',(req,res)=>{
     res.send(
@@ -12,7 +17,6 @@ app.get('/',(req,res)=>{
     )
 })
 app.use("/api/v1/",mainRouter);
-
 app.listen(5000,(req,res)=>{
     console.log("Listening...")
 })
